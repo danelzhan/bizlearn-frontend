905f88b60b8c4a7f0d72ed3ea2997622c24ff271
Daniel Zhang <danielz4734@gmail.com>
2025-08-22 11:12:42 -0700

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ function App() {
       {isAuthenticated ? <div/> : <Navigate to="/login" replace />}
       <Header />
         <Routes>
-          <Route path="/" element={<CoursesPage courses={demoCourse} percentage={percentage} />} />
+          <Route path={["/", "/authenticated"]} element={<CoursesPage courses={demoCourse} percentage={percentage} />} />
           <Route path="/course/:slug" element={<CoursePage percentage={percentage} />} />
           <Route path="/course/:slug/lesson/:id" element={<LessonPage userData={userData} setUserData={setUserData} />} />
           <Route path="/login" element={<LoginPage />} />
